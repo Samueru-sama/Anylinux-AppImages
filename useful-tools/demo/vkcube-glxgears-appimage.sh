131dce7ee7b5cb28790c71dc5a5f4984a4dccc9c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-13 23:32:11 -0400

    test

--- useful-tools/demo/vkcube-glxgears-appimage.sh
+++ useful-tools/demo/vkcube-glxgears-appimage.sh
@@ -37,7 +37,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-mesa --prefer-nano
+#./get-debloated-pkgs.sh --add-mesa --prefer-nano
 
 echo "Bundling AppImage..."
 echo "---------------------------------------------------------------"
