24de0dd11e18fb3bb3ad6e65502da26639fff3be
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-19 04:00:13 -0400

    test3

--- useful-tools/demo/gtk4-demo-appimage.sh
+++ useful-tools/demo/gtk4-demo-appimage.sh
@@ -40,7 +40,6 @@ echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
 ./get-debloated-pkgs.sh --add-common --prefer-nano
-./get-debloated-pkgs.sh mesa-mini
 
 echo "Bundling AppImage..."
 echo "---------------------------------------------------------------"
