5070bb879b2fae6b3670af3a3b7da3c7db3bbb75
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-09-12 08:46:30 -0400

    test

--- useful-tools/get-debloated-pkgs.sh
+++ useful-tools/get-debloated-pkgs.sh
@@ -215,7 +215,6 @@ if [ "$COMMON_PACKAGES" = 1 ]; then
 	set -- "$@" \
 		icu-mini         \
 		opus-mini        \
-		libxml2-mini     \
 		qt6-base-mini    \
 		gtk3-mini        \
 		gtk4-mini        \
